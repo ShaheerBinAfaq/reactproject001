11b89f06548f8e36ab01a5f3f573583c257e927e
ShaheerBinAfaq <shaheerafaq@gmail.com>
2020-06-03 13:56:11 +0500

    added ! to App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Shaeer was here</p>
+        <p>Shaeer was here!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
